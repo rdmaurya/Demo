19b2c2de45c60ad083dd48d469e4756c16a10c49
rdmaurya <159097513+rdmaurya@users.noreply.github.com>
2024-02-06 09:38:15 +0530

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+dict={
+    'mass' :[9,8,6,7,5],
+    'velocity' :[12,13,14,15,16]
+}
+df=pd.DataFrame(dict)
+st.table(df)
+st.title('Mass velocity Table')
